185d5ab992fcb862ac0ea82aacf199c6235dfbc6
Hossam Basiony <hbasiony@uw.edu>
2017-08-15 12:36:35 -0700

    added castlevania files

---
+++ Assets/Scripts/FollowCharacter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour {
+
+    public GameObject target;
+    private Camera cameraComponentReference;
+    public GameObject backGroundOfThisLevel;
+    private SpriteRenderer BackgroundSprite;
+    private Bounds BackGroundBounds;
+    float verticalExtent;
+    float horizontalExtend;
+    [HideInInspector]
+    public  float minX ;
+    [HideInInspector]
+    public  float maxX ;
+    float minY ;
+    float maxY ;
+    float backgroundWidth;
+    float backgroundHeight;
+    private GameObject oldTarget;
+    // Use this for initialization
+    void Start () {
+        cameraComponentReference = GetComponent<Camera>();
+       
+        AdjustCameraForBackgroundBoundaries();
+        oldTarget = backGroundOfThisLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (backGroundOfThisLevel != oldTarget)
+        {
+            AdjustCameraForBackgroundBoundaries();
+            oldTarget = backGroundOfThisLevel;
+        }
+
+       
+        //camera always centers on player unless it touches the boundaries of background
+            transform.position = new Vector3(Mathf.Clamp( target.transform.position.x,minX  + cameraComponentReference.orthographicSize * 1.4f,maxX - cameraComponentReference.orthographicSize * 1.4f), transform.position.y, transform.position.z);
+        
+    }
+    void AdjustCameraForBackgroundBoundaries()
+    {
+        //This gets called every time the background screen changes
+        BackgroundSprite = backGroundOfThisLevel.GetComponent<SpriteRenderer>();
+        BackGroundBounds = BackgroundSprite.bounds;
+        maxX = BackGroundBounds.max.x;
+        minX = BackGroundBounds.min.x;
+        
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public string axisName = "Horizontal";
+    private Animator anim;
+    private float defaultScaleZ;
+    private FollowCharacter CameraReference;
+    public GameObject currentBackground;
+    private SpriteRenderer currentbackgroundSprite;
+    private float spriteWidth;
+    private float spriteHeight;
+    // Use this for initialization
+    void Start()
+    {
+        currentbackgroundSprite = currentBackground.GetComponent<SpriteRenderer>();
+        anim = gameObject.GetComponent<Animator>();
+        defaultScaleZ = transform.position.z;
+        CameraReference = FindObjectOfType<FollowCharacter>();
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.max.x - GetComponent<SpriteRenderer>().bounds.min.x;
+        spriteHeight = GetComponent<SpriteRenderer>().bounds.max.y - GetComponent<SpriteRenderer>().bounds.min.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        print(currentbackgroundSprite.bounds.min.x);
+        print(currentbackgroundSprite.bounds.max.x);
+        //code for animation goes here
+        print(anim.GetBool("isMoving"));
+        if (Input.GetAxis(axisName) < 0f && Input.GetButtonUp("Attack"))
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, 180, transform.rotation.z, transform.rotation.w);
+            
+                anim.Play("Running");
+            
+        }
+        else if (Input.GetAxis(axisName) > 0f )
+        {
+            transform.rotation = new Quaternion(transform.rotation.x, 0, transform.rotation.z, transform.rotation.w);
+                anim.Play("Running");
+            
+        }
+        else if(Input.GetAxis(axisName) == 0)
+        {
+            anim.Play("Idle");    
+        }
+
+        
+      
+        //allows player to move as long as it is withing boundaries of camera
+        transform.position += new Vector3(( Input.GetAxis(axisName) * speed * Time.deltaTime),0,0);
+        transform.position = (new Vector3(Mathf.Clamp(transform.position.x, currentbackgroundSprite.bounds.min.x + spriteWidth, currentbackgroundSprite.bounds.max.x -spriteWidth), transform.position.y, transform.position.z));
+    }
+}
