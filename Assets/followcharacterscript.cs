8c043c2e13e3ccb0b5ff3f938cbf72aa543b10ad
MysteriousHercules <mysterioushercules@hotmail.com>
2017-08-17 21:30:53 -0700

    Basic Ai Code for the enemy

---
+++ Assets/followcharacterscript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followcharacterscript : MonoBehaviour {
+
+    public float range;
+    private PlayerScript currentPlayer;
+    private float distanceToPlayer;
+    public float speedOfEnemy;
+	// Use this for initialization
+	void Start () {
+        currentPlayer = FindObjectOfType<PlayerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(currentPlayer == null)
+        {
+            currentPlayer = FindObjectOfType<PlayerScript>();
+        }
+        distanceToPlayer = Vector2.Distance(transform.position, currentPlayer.transform.position);
+
+        if(distanceToPlayer < range)
+        {
+            followPlayer();
+        }
+	}
+    void followPlayer()
+    {
+        if(currentPlayer.transform.position.x < transform.position.x)
+        {
+            transform.position -= new Vector3(speedOfEnemy,0,0);
+            transform.SetPositionAndRotation(transform.position, new Quaternion(transform.rotation.x, 0, transform.rotation.z, transform.rotation.w));
+
+
+        }
+        else
+        {
+            transform.position += new Vector3(speedOfEnemy, 0, 0);
+            transform.SetPositionAndRotation(transform.position, new Quaternion(transform.rotation.x, 180, transform.rotation.z, transform.rotation.w));
+
+        }
+    }
+}
